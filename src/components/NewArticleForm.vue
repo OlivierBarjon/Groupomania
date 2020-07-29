e61a7ca361018076de8c50a6ca81e5244df438a9
Olivier Barjon <olivier.barjon@gmail.com>
2020-07-29 09:59:10 +0200

    finalisation POST nouvel article

--- src/components/NewArticleForm.vue
+++ src/components/NewArticleForm.vue
@@ -10,15 +10,15 @@
       ></b-form-input>
     </b-form-group>
 
-    <b-form-group id="input-group-2" label="Votre texte :" label-for="input-2" label-cols-sm="auto">
+    <b-form-group id="input-group-2" label="Commentaire :" label-for="input-2" label-cols-sm="auto">
       <b-form-textarea
         id="input-2"
         v-model="form.text"
         type="text"
         required
         placeholder="Votre texte ici"
-        rows ="8"
-        max-rows="20"
+        rows ="4"
+        max-rows="10"
       ></b-form-textarea>
     </b-form-group>
 
@@ -35,10 +35,10 @@
     ></b-form-file>
     <div v-if="form.file" class="mt-2">Fichier sélectionné : {{ form.file ? form.file.name : ''}}</div>
 </b-form-group>
- 
-
-    <b-button type="submit" variant="outline-dark">Envoyer</b-button>
-    <b-button type="reset" variant="outline-danger" >Effacer</b-button>
+    <p class="form__message-red">{{messageErreur}}</p>
+    <p class="form__message">{{message}}</p>
+    <b-button class="button" type="submit" variant="dark">Envoyer</b-button>
+    <b-button class="button-reset" size="sm" type="reset" variant="danger" >Effacer</b-button>
   </b-form>
 </template>
 
@@ -53,6 +53,7 @@ export default {
         file: null
       },
       show: true,
+      message:"",
     };
   },
   methods: {
@@ -60,7 +61,7 @@ export default {
       evt.preventDefault();
       const request = new XMLHttpRequest();
       const body = new FormData();
-      const image = this.form.file //new Blob([]/* , {type : "image/gif"} */);
+      const image = this.form.file //message perso : pas de Blob !!
       const article = {
         userId : this.form.userId,
         title : this.form.title,
@@ -68,10 +69,32 @@ export default {
       };
       body.append("article" , JSON.stringify(article));
       body.append("image", image);
-      console.log(this.form.file);
+      //console.log(this.form.file); //TEST
       request.open("POST", "http://localhost:3000/api/article/");
-      //request.setRequestHeader("Content-Type", "multipart/form-data")
+      //request.setRequestHeader("Content-Type", "multipart/form-data") // message perso : Le forcage  des headers produits des erreurs
       request.setRequestHeader('Authorization', 'Bearer ' + JSON.parse(localStorage.getItem('token')));
+      /////////////////////
+      new Promise((resolve, reject) => {
+              request.onreadystatechange = function () {
+                if (this.readyState == XMLHttpRequest.DONE && this.status == 201) {
+                  resolve(this.responseText);
+                }
+                if (this.status > 399) {
+                  reject();
+                }
+              };
+            })
+              .then(() => {
+                this.message =
+                  "Votre image Gif et son commentaire sont enregistrés. Vous allez être redirigé vers la page d'accueil dans quelques instants";
+                setTimeout(function () {
+                  document.location.href = "/";
+                }, 3000);
+              })
+              .catch(() => {
+                this.messageErreur = "Un problème est survenu durant l'envoi de votre message. Merci de recommencer plus tard.";
+              });
+      /////////////////////
       request.send(body);
     },
     onReset(evt) {
@@ -92,5 +115,17 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-
+.button {
+  margin: 1rem;
+}
+.button-reset {
+  margin-top: 0.4rem;
+}
+.form__message {
+  font-style: bold;
+  color: rgb(3, 143, 3);
+  &-red {
+    color: rgb(255,4,4);
+  }
+}
 </style>
